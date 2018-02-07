9e867c9bf9af87e62b10f5114c1c56c3ddb0d615
Jbaker8390 <jonathantuan02@gmail.com>
2018-02-06 22:19:31 -0800

    added 3 scenes, created folders for Fonts,Scripts, Sprites and _Scene. Added Start and Win Scenes and levelmanager.cs script.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name)
+    {
+        Debug.Log("Lvel load requested for : " + name);
+        //Loads the particular level
+        Application.LoadLevel(name);
+    }
+		
+    public void QuitRequest()
+    {
+        Debug.Log("I want to quit!");
+        Application.Quit();
+    }
+
+    public void ReturnToStart()
+    {
+        Application.LoadLevel("Start");
+    }
+	}
