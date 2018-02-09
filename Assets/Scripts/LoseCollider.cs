35466177f675ccc610d9016bb2f03acc2a76db7d
Jbaker8390 <jonathantuan02@gmail.com>
2018-02-08 22:41:43 -0800

    added basic trigger and collision collider for purpose of demonstartion

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class LoseCollider : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Trigger");   
+    }
+
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
 }
