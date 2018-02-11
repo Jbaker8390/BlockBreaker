bb8bf58259f15d1c644e3db737f97a383b7e868d
Jbaker8390 <jonathantuan02@gmail.com>
2018-02-10 21:34:44 -0800

    basic collider logic and added bricks to level

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+    public int maxHits;
+    private int timesHit;
+
+
+	// Use this for initialization
+	void Start () {
+        timesHit = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        timesHit++;
+    }
+}
