90cec52488de5d0c8593c09bcd38897d177decba
Jbaker8390 <jonathantuan02@gmail.com>
2018-02-10 20:00:45 -0800

    Created Rigidbody2 private variable, assigned initial velocity on mouse click

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
     public Paddle paddle;
-
+    private Rigidbody2D rb;
     private Vector3 paddleToBallVector;
 	// Use this for initialization
 	void Start () {
         //subtracts all 3 vector coordinates
         paddleToBallVector = this.transform.position - paddle.transform.position;
-	}
+        this.rb = this.GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         this.transform.position = paddle.transform.position + paddleToBallVector;
+        if(Input.GetMouseButtonDown(0))
+        {
+            print("mouse clicked, Launch Ball");
+
+            this.rb.velocity = new Vector2(2f, 10f);
+        }
 	}
 }
