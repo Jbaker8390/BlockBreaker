7f68c502d17d037f053d6c35d5f279b9ee3454c2
Jbaker8390 <jonathantuan02@gmail.com>
2018-02-10 19:38:25 -0800

    getting the transform difference between paddle and ball object

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+
+    private Vector3 paddleToBallVector;
+	// Use this for initialization
+	void Start () {
+        //subtracts all 3 vector coordinates
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
