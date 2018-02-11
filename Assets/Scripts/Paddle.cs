2d29e7add7a94dde096a400cf3a9fa4a2cb528dd
Jbaker8390 <jonathantuan02@gmail.com>
2018-02-10 19:29:55 -0800

    used mathF.clamp to constrain game place boundaries

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -11,9 +11,14 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
         //converts position into how many game units wide from 0 - 16
         float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-        print(mousePosInBlocks);
 
+        paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+
+        //the instance of the current game paddleScript of the paddle object
+        this.transform.position = paddlePos;
 	}
 }
