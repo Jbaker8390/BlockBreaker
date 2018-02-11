fab7a86f70005b91410b00155dd1e5e8b06a5e03
Jbaker8390 <jonathantuan02@gmail.com>
2018-02-10 20:06:35 -0800

    add logic for hasStarted to intiate launching ball when user has clicked a button

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,25 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour {
+public class Ball : MonoBehaviour
+{
     public Paddle paddle;
     private Rigidbody2D rb;
+    private bool hasStarted = false;
     private Vector3 paddleToBallVector;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         //subtracts all 3 vector coordinates
         paddleToBallVector = this.transform.position - paddle.transform.position;
         this.rb = this.GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        this.transform.position = paddle.transform.position + paddleToBallVector;
-        if(Input.GetMouseButtonDown(0))
-        {
-            print("mouse clicked, Launch Ball");
 
-            this.rb.velocity = new Vector2(2f, 10f);
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            //lock ball relative to the paddle
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+            //wait until mouse click to launch the ball
+            if (Input.GetMouseButtonDown(0))
+            {
+                print("mouse clicked, Launch Ball");
+                hasStarted = true;
+                this.rb.velocity = new Vector2(2f, 10f);
+            }
         }
-	}
+
+    }
 }
